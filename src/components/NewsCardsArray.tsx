0674509693e3c60b2ccff1978ae338daeacc51a1
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-06-10 10:20:45 -0300

    feat: add map to load news card array

--- src/components/NewsCardsArray.tsx
+++ src/components/NewsCardsArray.tsx
@@ -1,5 +1,6 @@
 import type { NewsCardType } from "../types/news";
 import NewsCard from "./NewsCard";
+import '../styles/newsCardsArray.css'
 
 interface NewsCardsArrayProps {
     news: NewsCardType[];
@@ -9,15 +10,16 @@ function NewsCardsArray ({news}: NewsCardsArrayProps) {
     return (
         <div className="news-container">
             {
-              news.map((news) => (
-                <NewsCard 
-                    key={news.id}
-                    id={news.id}
-                    title={news.title}
-                    summary={news.title}
-                    reference={news.reference}
-                />
-              ))
+                news.map((news) => (
+                    <NewsCard 
+                        key={news.id}
+                        id={news.id}
+                        imagePath={news.imagePath}
+                        title={news.title}
+                        summary={news.summary}
+                        subject={news.subject}
+                    />
+                ))
             }
         </div>
     );
