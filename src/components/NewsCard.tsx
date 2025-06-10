28e5764f23b9c928b3ace26a12257a7cf3efafda
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-06-10 10:20:02 -0300

    feat: import style and add imagePath prop

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,11 +1,13 @@
 import type { NewsCardType } from "../types/news";
+import '../styles/newsCard.css';
 
-function NewsCard({title, summary, reference}: NewsCardType) {
+function NewsCard({imagePath, title, summary, subject}: NewsCardType) {
     return(
         <div className="news-card">
+            <img className="card-image" src={imagePath} alt="" />
             <h1 className="card-title">{title}</h1>
             <p className="card-summary">{summary}</p>
-            <small className="card-reference">Fonte: {reference}</small>
+            <small className="card-subject">{subject}</small>
         </div>
     );
 }
